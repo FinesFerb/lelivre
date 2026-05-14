e83d3add530a1365e91e1d4f8d323f52a817fa9a
Daniel <kargapoltsev2016@gmail.com>
2026-05-14 19:39:10 +0400

    То же самое с register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
 
 @section('content')
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg" style="align-self: center;">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gray-100 dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg" style="align-self: center;">
             <form method="POST" action="{{ route('register') }}">
                 @csrf
 
